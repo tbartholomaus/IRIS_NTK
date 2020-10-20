de9fb6eab6c38f4295a55e68969d5d53aeeb9993
Tim Bartholomaus <24902730+tbartholomaus@users.noreply.github.com>
2020-10-20 14:28:26 -0700

    spectrogram corrections/improvements

--- bin/ntk_spectrograms.py
+++ bin/ntk_spectrograms.py
@@ -6,24 +6,36 @@ Created on Fri Sep 25 17:20:56 2020
 @author: timb
 """
 
+import os
 from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 #%%
-
-datatype = 'betaSquare'
+# datatype options are:
+    #   betaSquare  phiHH       phiVH       powerEW     powerLambda powerNS     powerUD     thetaH      thetaV
+datatype = 'powerLambda'
 # datatype = 'thetaH'
-net_sta_loc = 'NM.SLM.--'
-chans = 'BHZ_BHE_BHN'
-filename = net_sta_loc + '.' + chans + '.2008-12-31T00_00_00.2009-01-10T00_00_00.frequency.txt'
+
+# net_sta_loc = 'NM.SLM.--'
+# chans = 'BHZ_BHE_BHN'
+# filename = net_sta_loc + '.' + chans + '.2008-12-31T00_00_00.2009-01-10T00_00_00.frequency.txt'
+
+net_sta_loc = 'ZQ.RTBD.--'
+# net_sta_loc = 'LM.BBEL.--'
+chans = 'HHZ_HHE_HHN'
+filename = net_sta_loc + '.' + chans + '.*.frequency.txt'
 
 current_path = Path.cwd()
 data_path = current_path / '..' / Path('data/POLAR') / Path(net_sta_loc) / Path(chans) / Path(datatype)
 
 colnames = ['date', 'time', 'freq', datatype]
-data = pd.read_csv(data_path / filename, sep='\t', names=colnames, parse_dates=[[0,1]])
+# data = pd.read_csv(data_path / filename, sep='\t', names=colnames, parse_dates=[[0,1]])
+path_and_all_files = list(data_path.glob(filename))
+path_and_all_files.sort(key=os.path.getmtime, reverse=True) # Sort by descending modified time, to allow for working with the most recent version
+path_and_file = path_and_all_files[0]
+data = pd.read_csv(path_and_file, sep='\t', names=colnames, parse_dates=[[0,1]])
 print(data)
 
 #%%
@@ -33,11 +45,13 @@ freq_all = data['freq'].to_numpy()
 freq_all_log = np.log10(freq_all)
 data_val_column = data[datatype].to_numpy()
 
-date_times =np.unique(date_time_all)
-freqs_log =np.unique(freq_all_log)
+delta_date_times = np.mean(   np.diff( np.unique(date_time_all) )   )
+date_times = np.arange(np.min(date_time_all), np.max(date_time_all), delta_date_times)
+
+# Keep commented date_times = np.unique(date_time_all) # Keep commented
+freqs_log = np.unique(freq_all_log)
 
 delta_freqs_log = np.mean(np.diff(freqs_log))
-delta_date_times = np.mean(np.diff(date_times))
 
 # inspired/lifted from http://chris35wills.github.io/gridding_data/
 i = ((freq_all_log - freqs_log[0]) / delta_freqs_log).astype(int) # y locations as grid indices
@@ -51,8 +65,14 @@ data_val_mesh[i,j] = data_val_column
 # data_val_mesh, yi, xi = np.histogram2d(freq_all_log, date_time_all, bins=(len(freqs_log), len(date_times)), 
 #                             weights=data_val_column, normed=False)
 
+if 'theta' in datatype or 'phi' in datatype:
+    cmap = 'twilight'
+else:
+    cmap = 'viridis'
+
 fig, ax = plt.subplots()
-img = ax.pcolormesh(date_times, 10**freqs_log, data_val_mesh)
+img = ax.pcolormesh(date_times, 10**freqs_log, data_val_mesh, shading='auto', 
+                    cmap=cmap)
 ax.set_yscale('log')
 fig.colorbar(img)
 fig.autofmt_xdate()
